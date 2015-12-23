fdffa75676818acc8bbfea86913d8ce9d22923be
Krzysztof Jordan <krisjj123@gmail.com>
2015-12-23 12:20:44 -0500

    fixed update of trial history

--- code/updateTrialHistory.m
+++ code/updateTrialHistory.m
@@ -4,16 +4,16 @@ FAKE = 0;
 CORRECT = 1;
 INCORRECT = 2;
 
+updatedTrialHistory = trialHistory;
+updatedIndex = currentIndex;
+
 if isCorrectResponse          
      updatedTrialHistory = horzcat(trialHistory, CORRECT);
-     historySize = length(trialHistory);
+     historySize = length(updatedTrialHistory);
      % two correct in a row, become harder
-     if  historySize > 1 && trialHistory(historySize) == CORRECT && trialHistory(historySize - 1) == CORRECT
-         updatedTrialHistory = horzcat(trialHistory, FAKE);
+     if  historySize > 1 && updatedTrialHistory(historySize) == CORRECT && updatedTrialHistory(historySize - 1) == CORRECT
+         updatedTrialHistory = horzcat(updatedTrialHistory, FAKE);
          updatedIndex = min(currentIndex + 1, maxIndex);
-     else
-         % one correct in a row, keep the same index
-         updatedIndex = currentIndex;
      end
  else % one incorrect, become easier
      updatedTrialHistory = horzcat(trialHistory, INCORRECT);
